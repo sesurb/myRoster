0bf2ee494aae8428e7eaccfe994b19bc9c855eed
Scott Surber <s.surber@msn.com>
2016-02-22 15:25:51 -0700

    Initial
    Intitial

---
+++ MyRoster/app/controller/rosterController.js
@@ -0,0 +1,17 @@
+﻿app.controller("rosterController", function ($scope, DataService) {
+    $scope.players = [];
+    $scope.addPlayer = function () {
+        var player = {
+            name: $scope.newplayer.name,
+            number: $scope.newplayer.number,
+            position: $scope.newplayer.position
+
+        }
+        $scope.players.push(player)
+        console.log($scope.players);
+    };
+    $scope.removePlayer = function () {
+
+    };
+
+});
---
+++ MyRoster/app/services/data.services.js
@@ -0,0 +1,16 @@
+﻿app.factory('DataService',function($http){
+    var players = [];
+    var service = {
+        loadplayers: function(){
+        
+        },
+        getAllPlayers: function(){
+            $http.get('http://bcw-getter.herokuapp.com/?url=http%3A%2F%2Fapi.cbssports.com%2Ffantasy%2Fplayers%2Flist%3Fversion%3D3.0%26SPORT%3Dfootball%26response_format%3Djson')
+            .success(function (data) {
+                players = data;
+                console.log(players);
+            });
+        }
+    }
+    return service;
+});
